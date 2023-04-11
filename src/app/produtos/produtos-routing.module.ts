78e343870f686d7905c3fd708ee8159200199265
Weldon <weldonprogrammer2022@gmail.com>
2023-04-11 16:38:23 -0300

    criação do component 404

--- src/app/produtos/produtos-routing.module.ts
+++ src/app/produtos/produtos-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProdutosComponent } from './produtos.component';
+import { DescricaoProdutosComponent } from './descricao-produtos/descricao-produtos.component';
 
-const routes: Routes = [{ path: '', component: ProdutosComponent }];
+const routes: Routes = [
+  { path: '', component: ProdutosComponent },
+  { path: ':id', component: DescricaoProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
