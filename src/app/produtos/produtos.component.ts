578fe1c719a8580d37776af930f3971668424b1f
Weldon <weldonprogrammer2022@gmail.com>
2023-04-13 17:24:41 -0300

    injetando activatedRoute

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IProduto } from '../produtos';
 import { ProdutosService } from '../produtos.service';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -13,12 +14,31 @@ export class ProdutosComponent {
   produtos: IProduto [] | undefined;
 
   constructor(
-    private produtosService: ProdutosService
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute
 
   ){}
 
   ngOnInit(): void{
-    this.produtos = this.produtosService.getAll();
+
+    const produtos = this.produtosService.getAll();
+
+    this.route.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLowerCase();
+     
+     
+      
+    if( descricao){
+      this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+      return;
+    }
+    
+    this.produtos = produtos;
+
+    });
+
+    
+
   }
 
 } 
