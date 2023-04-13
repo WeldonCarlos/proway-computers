1594017003099692748901938152d0e500a494be
Weldon <weldonprogrammer2022@gmail.com>
2023-04-13 10:10:47 -0300

    Criando toda a funcionalidade da pagina de descrição do produto

--- src/app/produtos/descricao-produtos/descricao-produtos.component.ts
+++ src/app/produtos/descricao-produtos/descricao-produtos.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CarrinhoService } from 'src/app/carrinho.service';
+import { NotificacaoService } from 'src/app/notificacao.service';
+import { IProduto, IProdutoCarrinho } from 'src/app/produtos';
+import { ProdutosService } from 'src/app/produtos.service';
 
 @Component({
   selector: 'app-descricao-produtos',
@@ -6,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./descricao-produtos.component.css']
 })
 export class DescricaoProdutosComponent {
+
+  estoque = 1;
+  quantidade= 0 ;
+  produto: IProduto | undefined;
+  constructor(
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute,
+    private notificacaoService: NotificacaoService,
+    private carrinhoService: CarrinhoService,
+  ) { }
+
+  ngOnInit() {
+
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtosService.getOne(produtoId);
+  }
+  adicionarAoCarrinho() {
+    this.notificacaoService.notificar("O produto foi adicionado ao carrinho!")
+    const produto: IProdutoCarrinho = {
+      ...this.produto!,
+      quantidade: this.quantidade
+    }
+    this.carrinhoService.adicionarAoCarrinho(produto)
+  };
 
 }
